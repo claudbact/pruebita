bed0455e7cfb059968b0c01b0b8a7fb971d2fb34
Brenda Tafur <tafurbrenda@gmail.com>
2020-10-06 13:04:00 -0500

    "Agregué usuario"

---
+++ admin/usuario.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from flask import Blueprint, render_template, session, request, redirect
+
+#objeto que tiene la subaplicacion
+view = Blueprint('usuario_bludprint', __name__)
+
+@view.route('/inicio', methods=['GET'])
+def registro():    
+    locals = {
+        'message': '',
+        
+    }
+    return render_template(
+        '/usuario/inicio.html',
+        locals=locals # acá seteamos una variable en nuestro template, en el tempalte tiene que coincider con el nombre locals, yy locals es undiccionario que en una de sus lavest tiene 
+    ), 200
+
+
--- main/blueprints.py
+++ main/blueprints.py
@@ -6,6 +6,7 @@ from access.views2 import view as access_view2
 from admin.views import view as admin_view
 from admin.registro import view as registro_view
 from admin.objeto import view as objeto_view
+from admin.usuario import view as usuario_view
 
 def register(APP):
     # register blueprints
@@ -14,3 +15,4 @@ def register(APP):
     APP.register_blueprint(admin_view)
     APP.register_blueprint(registro_view)
     APP.register_blueprint(objeto_view)
+    APP.register_blueprint(usuario_view)
