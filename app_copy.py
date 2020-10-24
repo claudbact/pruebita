28c026a5db91f2f325d7f87d6769c618a4965784
Cielo Escobedo <20172103@aloe.ulima.edu.pe>
2020-10-24 00:13:29 -0500

    "Se arreglo :) el login"

---
+++ app_copy.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''from main.test import countries_list, get_country_name_by_id, usuario_list,get_password_by_id,get_rol_by_id
+
+if __name__ == '__main__':'''
+    #countries_list()
+    #get_country_name_by_id(25, 'Russia')
+    #usuario_list()
+#get_password_by_id('20172103')
+
+    #print (get_rol_by_id('20172103'))
+
+from main.application import APP
+
+if __name__ == '__main__':
+
+ APP.run(
+      debug=True,
+   )
+
+#####################################################
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
